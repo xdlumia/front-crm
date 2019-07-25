3ed7b74f6b5d2ec3d20903c12cc832bd6ab0c0d4
xdlumia <xdq@live.cn>
2019-07-25 09:23:54 +0800

    修改scrolllist

--- src/utils/global-config.js
+++ src/utils/global-config.js
@@ -2,18 +2,20 @@ import api from './api'
 import local from './localStorage'
 import router from './router'
 import utils from './utils'
+import commonMixin from './commonMixin'
 import {
-    $toast,
-    $message
+	$toast,
+	$message
 } from './message'
 
 export default {
-    install: function (vm) {
-        vm.prototype.$local = local
-        vm.prototype.$api = api
-        vm.prototype.$routing = router
-        vm.prototype.$toast = $toast
-        vm.prototype.$message = $message
-        vm.prototype.$utils = utils
-    }
+	install: function (vm) {
+		vm.mixin(commonMixin)
+		vm.prototype.$local = local
+		vm.prototype.$api = api
+		vm.prototype.$routing = router
+		vm.prototype.$toast = $toast
+		vm.prototype.$message = $message
+		vm.prototype.$utils = utils
+	}
 }
