30434d2533050e382c84d59711998049dc18fa30
muluo <1097198549@qq.com>
2019-07-25 09:20:27 +0800

    修改 image-pick 组件

--- src/api/seeExternService.js
+++ src/api/seeExternService.js
@@ -1,13 +1,14 @@
 const MOCK_ADDRESS = 'http://10.168.1.145:9024'
 const IS_MOCK = false
 export default {
-    __mockAddress: MOCK_ADDRESS,
-    getPrivateOssTicket: {
-        url: '/ossv/getOssTicketV',
-        mock: IS_MOCK
-    },
-    getPublicOssTicket: {
-        url: '/oss/getOssTicket',
-        mock: IS_MOCK
-    }
+	__mockAddress: MOCK_ADDRESS,
+	getPrivateOssTicket: {
+		url: '/ossv/getOssTicketV',
+		mock: IS_MOCK
+	},
+	getPublicOssTicket: {
+		url: '/oss/getOssTicket',
+		mock: IS_MOCK
+	}
+
 }
