ec35df2042a504e1e483c182a01e65bddd0e5f59
muluo <1097198549@qq.com>
2019-07-26 17:05:41 +0800

    添加 底部菜单 样

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script>
-	export default {
-        /** 以下是应用生命周期 */
-        // 当uni-app 初始化完成时触发（全局只触发一次）
-		onLaunch: function() {
-			console.log('App Launch')
-        },
-        // 当 uni-app 启动，或从后台进入前台显示
-		onShow: function() {
-            console.log('onShow')
-        },
-        // 当 uni-app 从前台进入后台
-		onHide: function() {
-			console.log('App Hide')
-		}
+export default {
+	/** 以下是应用生命周期 */
+	// 当uni-app 初始化完成时触发（全局只触发一次）
+	onLaunch: function () {
+		console.log('App Launch')
+	},
+	// 当 uni-app 启动，或从后台进入前台显示
+	onShow: function () {
+		console.log('onShow')
+	},
+	// 当 uni-app 从前台进入后台
+	onHide: function () {
+		console.log('App Hide')
 	}
+}
 </script>
 
 <style>
@@ -24,5 +24,14 @@
   -moz-transition: width 2s;
   -webkit-transition: width 2s;
   -o-transition: width 2s;
+}
+/* 底部浮动菜单*/
+.footer-fixed-menu{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 10px 15px;
+    box-sizing: border-box;
 }
 </style>
