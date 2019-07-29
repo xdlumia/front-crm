c1e3e49f72582fac8400a1fe8b58a56d6ab3a6de
徐贺 <15210614992@users.noreply.github.com>
2019-07-29 15:30:34 +0800

    新建日程改样式

--- src/pages/index/scheduleAdd.vue
+++ src/pages/index/scheduleAdd.vue
@@ -5,15 +5,16 @@
 <template>
     <div>
         <NavBar title="新建日程" />
+
         <div class="d-bg-schedule"></div>
-			<i-input v-model="value1" placeholder="日程主题" class='wfull'/>
+			<i-input label="日程主题" v-model="value1" class='wfull'/>
         <div class="d-bg-schedule"></div>
+
         <div class="d-bg-white wfull d-flex" style="align-items:center;height: 48px;">
-            <div class="ml15 w100">位置</div>
-            <i-input v-model="value1" placeholder="点击输入" class='wfull'/>
-                <div @click="fChooseMap" class="ml15 ac" style="border-left: 1px solid #F2F2F2;line-height: 48px;width:100px;">
-                    <i-icon type="coordinates" size="22" color="#999" />
-                </div>
+            <i-input label="位置" v-model="value1" placeholder="点击输入" class='wfull'/>
+            <div @click="fChooseMap" class="ml15 ac" style="border-left: 1px solid #F2F2F2;line-height: 48px;width:100px;">
+                <i-icon type="coordinates" size="22" color="#999" />
+            </div>
         </div>
         <div class="d-bg-schedule"></div>
 
@@ -22,52 +23,62 @@
                 开始时间 {{value1}}
             </view>
         </picker> -->
-        <view class="uni-list">
-            <view class="uni-list-cell" style="height:50px;align-items:center;border-top:none">
-                <view class="uni-list-cell-left">
-                    开始
-                </view>
-                <view class="uni-list-cell-db">
-                    <picker mode="date" v-model="date" :start="startDate" :end="endDate" @change="bindDateChange">
-                        <view class="uni-input">{{date}}</view>
-                    </picker>
-                </view>
-            </view>
-        </view>
 
-        <view class="uni-list">
-            <view class="uni-list-cell" style="height:50px;align-items:center;border-top:none">
-                <view class="uni-list-cell-left">
-                    结束
-                </view>
-                <view class="uni-list-cell-db">
-                    <picker mode="date" v-model="date" :start="startDate" :end="endDate" @change="bindDateChange">
-                        <view class="uni-input">{{date}}</view>
-                    </picker>
-                </view>
-            </view>
-        </view>
+        <picker mode="date" :value="timeIndex" @change='bindDateChange($event, "upTypeIndex")'>
+            <div class="form-row d-center">
+                <div class="f13 d-text-black form-row-item form-row-label">
+                    <span class='d-text-red'>*</span>开始
+                </div>
+                <div class="d-cell mr10 form-row-item">
+                    <input type="text" class='f12 d-text-gray' placeholder="">
+                </div>
+            </div>
+        </picker>
 
-        <view class="uni-list">
-            <view class="uni-list-cell" style="height:50px;align-items:center;border-top:none">
-                <view class="uni-list-cell-left">
-                    提醒
-                </view>
-                <view class="uni-list-cell-db">
-                    <picker v-model="date" :range="array" @change="bindDateChange">
-                        <view class="uni-input">{{array[index]}}</view>
-                    </picker>
-                </view>
-            </view>
-        </view>
+        <picker mode="date" :value="timeIndex" @change='bindDateChange($event, "upTypeIndex")'>
+            <div class="form-row d-center">
+                <div class="f13 d-text-black form-row-item form-row-label">
+                    <span class='d-text-red'>*</span>结束
+                </div>
+                <div class="d-cell mr10 form-row-item">
+                    <input type="text" class='f12 d-text-gray' placeholder="">
+                </div>
+            </div>
+        </picker>
 
-        <i-cell-group>
-            <i-cell title="参与人"></i-cell>
-        </i-cell-group>
+        <picker mode="date" :value="timeIndex" @change='bindDateChange($event, "upTypeIndex")'>
+            <div class="form-row d-center">
+                <div class="f13 d-text-black form-row-item form-row-label">
+                    <span class='d-text-red'>*</span>提醒
+                </div>
+                <div class="d-cell mr10 form-row-item">
+                    <input type="text" class='f12 d-text-gray' placeholder="">
+                </div>
+            </div>
+        </picker>
+
+        <div class="form-row d-center">
+            <div class="f13 d-text-black form-row-item form-row-label">
+                <span class='d-text-red'>*</span>参与人
+            </div>
+            <div class="d-cell mr10 form-row-item">
+                <input type="text" disabled class='f12 d-text-gray' placeholder="">
+            </div>
+        </div>
+
         <div class="d-bg-schedule"></div>
+
         <a url='/pages/index/affiliated'>
-            <i-cell title="关联业务"  is-link></i-cell>
+            <div class="form-row d-center">
+                <div class="f13 d-text-black form-row-item form-row-label">
+                    <span class='d-text-red'>*</span>关联业务
+                </div>
+                <div class="d-cell mr10 form-row-item">
+                    <input type="text" disabled class='f12 d-text-gray' placeholder="">
+                </div>
+            </div>
         </a>
+
         <i-button type="primary">确 定</i-button>
 
     </div>
@@ -81,7 +92,8 @@ export default {
 		return {
 			value1: '',
 			date: '',
-			array: ['中国', '美国', '巴西', '日本']
+			array: ['中国', '美国', '巴西', '日本'],
+			timeIndex: []
 		}
 	},
 	computed: {
@@ -135,4 +147,28 @@ export default {
 
 <style scoped lang="scss">
 .d-bg-schedule{height:13px;align-items: center;background:#f5f6fa ;}
+.form-row{
+        margin-top: -1px;
+        border-top: 1px solid #f2f2f2;
+        border-bottom: 1px solid #f2f2f2;
+        height: 51px;
+        position: relative;
+        &:after{
+            content: " ";
+            display: inline-block;
+            width: 6px;
+            height: 6px;
+            position: absolute;
+            top: 50%;
+            right: 15px;
+            border-width: 2px 2px 0 0;
+            border-color: #dddee1;
+            border-style: solid;
+            transform: translateY(-50%) matrix(.71, .71, -.71, .71, 0, 0)
+        }
+        .form-row-label{
+            width: 110px;
+            text-indent: 15px;
+        }
+    }
 </style>
