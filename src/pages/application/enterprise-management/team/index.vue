b71daed1e8705f04d6dd97446782ae1d3b90c222
wangyanlong <2631748820@qq.com>
2019-07-30 18:49:32 +0800

    wangyanlong
    Signed-off-by: wangyanlong <2631748820@qq.com>

--- src/pages/application/enterprise-management/team/index.vue
+++ src/pages/application/enterprise-management/team/index.vue
@@ -2,12 +2,12 @@
     <view>
         <NavBar title="团队申请" />
         <view class="uni-flex uni-column">
-            <view class="flex-item flex-item-V bb uni-flex uni-row p15" v-for="(item) in teamDatas" :key="item.is">
+            <view class="flex-item flex-item-V bb uni-flex uni-row p15" v-for="(item) in teamDatas" :key="item.id">
                  <view class="flex-item width20 p10">
                     <image class="ba" style="height: 51px;width: 51px;" src="/static/img/my-default.png"></image>
                 </view>
-                <view class="flex-item width32">
-                    <view class="f16">{{item.name}}</view>
+                <view class="flex-item width32" @click="info(item)">
+                    <view class="f14">{{item.name}}</view>
                     <view class="d-text-qgray">来源</view>
                     <view>由<span style="color:#5081F5;">{{item.inviter}}</span>邀请加入</view>
                     <view class="d-text-qgray">申请理由</view>
@@ -37,21 +37,27 @@ export default {
 					id: 0,
 					pic: '../../../../../dist/dev/mp-weixin/static/img/my-default.png',
 					name: '付建达',
+					phone: '16619941016',
 					inviter: '方胜利',
+					verifier: 'lili',
 					applyReason: 'aaaaaaaaaaaaaaaaaaaaa'
 				},
 				{
-					id: 0,
+					id: 1,
 					pic: '../../../../../dist/dev/mp-weixin/static/img/my-default.png',
 					name: '高阳',
+					phone: '16619941016',
 					inviter: '高德勇',
+					verifier: 'lili',
 					applyReason: 'bbbbbbbbbbbbbbbbbbbbb'
 				},
 				{
-					id: 0,
+					id: 2,
 					pic: '../../../../../dist/dev/mp-weixin/static/img/my-default.png',
 					name: '梵高先生',
+					phone: '16619941016',
 					inviter: '李丽兰',
+					verifier: 'lili',
 					applyReason: 'cccccccccccccccccccccccccccccccc'
 				}
 			]
@@ -61,9 +67,16 @@ export default {
 
 	},
 	methods: {
+		// 编辑
 		toEditor () {
 			uni.navigateTo({
 				url: 'editor/index?isEditor=1'
+			})
+		},
+		// 详情
+		info (item) {
+			uni.navigateTo({
+				url: 'info?item=' + JSON.stringify(item)
 			})
 		}
 	},
---
+++ src/pages/application/enterprise-management/team/info.vue
@@ -0,0 +1,71 @@
+<template>
+    <view>
+        <NavBar title="详细信息" />
+        <view class="uni-form-item" style="height: 10px;background: #F9F9F9;"></view>
+        <view class="uni-flex uni-column">
+            <view class="flex-item flex-item-V uni-flex uni-row bb pt10">
+                    <view class="flex-item ml10">
+                    <image class="ba" style="height: 51px;width: 51px;" :src=pic></image>
+                    </view>
+                    <view class="flex-item ml10">
+                        <view class="m5 f14">{{name}}</view>
+                        <view class="m5 d-text-qgray">+86-{{phone}}</view>
+                    </view>
+                    <view class="ml10"></view>
+            </view>
+            <view class="flex-item flex-item-V bb pl20">
+                <view class="pl50 m5 pl10">由<span style="color:#5081F5;">{{inviter}}</span>邀请加入</view>
+                <view class="pl50 m5 pl10">由<span style="color:#5081F5;">{{verifier}}</span>同意加入</view>
+            </view>
+            <view class="flex-item flex-item-V pl20">
+                <view class="pl50 m5 pl10 d-text-qgray">申请理由</view>
+                <view class="pl50 m5 pl10">{{applyReason}}</view>
+            </view>
+            <view class="uni-form-item" style="height: 10px;background: #F9F9F9;"></view>
+            <view class="flex-item flex-item-V">
+                <button class="width90 f15 p5 h30" style="border: none;" plain="true">已同意</button>
+            </view>
+            <view class="uni-form-item" style="height: 10px;background: #F9F9F9;"></view>
+        </view>
+    </view>
+</template>
+<script>
+export default {
+	data () {
+		return {
+			id: 0,
+			pic: '',
+			name: '呼呼呼',
+			phone: '18233279575',
+			inviter: '浏览量',
+			verifier: '快快快',
+			applyReason: '行行行'
+		}
+	},
+	onLoad (option) {
+		if (option.item) {
+			let item = JSON.parse(option.item)
+			this.pic = item.pic
+			this.name = item.name
+			this.phone = item.phone
+			this.inviter = item.inviter
+			this.verifier = item.verifier
+			this.applyReason = item.applyReason
+		}
+	}
+}
+</script>
+<style>
+    .width20{
+        width: 20%;
+    }
+    .width32{
+        width: 32%;
+    }
+    .width48{
+        width: 48%;
+    }
+    .width90{
+        width: 90%;
+    }
+</style>
