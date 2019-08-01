f8f6c07d984fb56013838f100d9f83e252fb7422
xdlumia <xdq@live.cn>
2019-08-01 14:38:58 +0800

    no message

---
+++ src/pages/chance/detail/components/info-contact.vue
@@ -0,0 +1,43 @@
+<!--
+/**
+* @author 王晓冬
+* @name 联系人
+* @date 2019年7月299日
+**/
+-->
+<template>
+    <div>
+        <mPanel title="联系人" color="#7765cc" url="/pages/contact/index?select=1">
+            <div class="detail-list">
+                <div class="list-title">
+                    <title>黄玉里</title> <span class="d-text-qgray">总精力</span>
+                    <span @click="callPhone(18210256005)" class="fr iconfont iconcall f18 d-text-gray" ></span>
+                </div>
+                <p class="f12 d-elip d-text-qgray">华为技术有限公司</p>
+            </div>
+        </mPanel>
+    </div>
+</template>
+
+<script>
+export default {
+	props: ['busId', 'busType'],
+	components: {
+		// mPager
+	},
+	data () {
+		return {
+			list: []
+		}
+	},
+	onLoad (option) {
+	},
+	methods: {
+	},
+	created () {}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/chance/detail/components/info-schedule.vue
@@ -0,0 +1,41 @@
+<!--
+/**
+* @author 王晓冬
+* @name 日程
+* @date 2019年7月299日
+**/
+-->
+<template>
+    <div>
+        <mPanel top="10" title="日程" color="#4889f4" url="/pages/index/scheduleAdd">
+            <div class="detail-list">
+                <p class="f13 d-elip d-text-gray">明天粉色扥发东风东</p>
+                <p class="f12 d-elip d-text-gray">2019/01/03 13:20 至2019/01/31 15:20</p>
+                <p class="f12 d-elip d-text-blue" style="background-colr:#efefef"><i-icon type="coordinates" size="18" color="#4788F4" />我是尝尝的遗传地址</p>
+            </div>
+        </mPanel>
+    </div>
+</template>
+
+<script>
+export default {
+	props: ['busId', 'busType'],
+	components: {
+		// mPager
+	},
+	data () {
+		return {
+			list: []
+		}
+	},
+	onLoad (option) {
+	},
+	methods: {
+	},
+	created () {}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
