2d19ae6d9108642ac20c94dfec9e570a534d4f86
wangyanlong <2631748820@qq.com>
2019-08-09 14:56:02 +0800

    修改选择部门回调方法
    Signed-off-by: wangyanlong <2631748820@qq.com>

--- src/pages/application/enterprise-management/organizational-structure.vue
+++ src/pages/application/enterprise-management/organizational-structure.vue
@@ -67,21 +67,8 @@ export default {
 		},
 		// 确定
 		makeSure () {
-			let pages = getCurrentPages()
-			// 获取当前页面js里面的pages里的所有信息。
-			let prevPage = pages[ pages.length - 2 ]
-			// prevPage 是获取上一个页面的js里面的pages的所有信息。 -2 是上一个页面，-3是上上个页面以此类推。
-			prevPage.setData({ // 将我们想要传递的参数在这里直接setData。上个页面就会执行这里的操作。
-				deptId: this.depts[this.current].id,
-				deptName: this.depts[this.current].deptName,
-				backFromOrg: true
-			})
-			// 上一个页面内执行setData操作，将我们想要的信息保存住。当我们返回去的时候，页面已经处理完毕
-			// 最后就是返回上一个页面。
-			uni.navigateBack({
-				delta: 1 // 返回上一级页面。
-			})
-			// 此时页面数据已经改变为我们传递过来的数据。如果想要返回之后处理这些数据，那么要在onShow函数里执行，因为我们执行的是返回，所以不会触发onLoad函数，所以我们要在onShow里执行我们想要使用的函数。
+			uni.$emit('backFromOrg', JSON.stringify(this.depts[this.current]))
+			this.$routing.navigateBack()
 		}
 	}
 }
