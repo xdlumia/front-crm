7f0dfcaf9388edbcc51ba928e93226a1c06a64eb
muluo <1097198549@qq.com>
2019-08-23 16:43:36 +0800

    公海池没有公海 新建客户 限制

--- src/pages/highseas/index.vue
+++ src/pages/highseas/index.vue
@@ -55,9 +55,9 @@
 			</scroll-list>
 		</div>
 		<div class="footer-fixed-menu d-center d-bg-white" v-if="!select">
-			<a :url="'/pages/client/add-client?poolId=' + pool.id" class="d-cell al">
+			<div  class="d-cell al" @click='addClient'>
 				<uni-icon type='plus' size='16' color='#666' /><span class="ml5 f13  d-text-gray">新建客户</span>
-			</a>
+			</div>
 			<a url='./setting' class="d-cell ar">
 				<i-icon type='setup' size='18' color='#666' /><span class="ml5 f13  d-text-gray">管理</span>
 			</a>
@@ -160,6 +160,12 @@ export default {
 		}
 	},
 	methods: {
+		addClient () {
+			if (!this.pool.id) {
+				return this.$utils.toast.text('还没有客户公海')
+			}
+			this.$routing.navigateTo(`/pages/client/add-client?poolId=${this.pool.id || ''}`)
+		},
 		//
 		handlerClient (item, index) {
 			if (!this.select) {
