a1e4a9e5b1d59b39edba2e1072eca9ec526f4838
徐贺 <15210614992@users.noreply.github.com>
2019-08-23 18:46:35 +0800

    应用界面icon调整

--- src/pages/application/index.vue
+++ src/pages/application/index.vue
@@ -4,30 +4,30 @@
         <div class="hfull wfull">
             <div class="flexcenter hfull wfull pt20" style='background: #FFF;'>
                 <a url='/pages/client/index' open-type='switchTab' class="flexalone flexcenter flexconm d-text-black">
-                    <i class="iconfont icontongyekehuguanli" style="color: #FF9A26;"></i>
-                    <span>客户管理</span>
+                    <i class="iconfont icontongyekehuguanli" style="color: #FF9A26;line-height:1"></i>
+                    <span class="mt10">客户管理</span>
                 </a>
                 <a url='/pages/chance/index' open-type='switchTab' class="flexalone flexcenter flexconm d-text-black">
-                    <i class="iconfont iconqian" style="color: #4889F4;"></i>
-                    <span>销售机会</span>
+                    <i class="iconfont iconqian" style="color: #4889F4;line-height:1"></i>
+                    <span class="mt10">销售机会</span>
                 </a>
                 <a url='/pages/highseas/index' class="flexalone flexcenter flexconm d-text-black">
-                    <i class="iconfont f20 icongengduo1" style="color: #00CA8D;"></i>
-                    <span>客户公海池</span>
+                    <i class="iconfont icongengduo1" style="color: #00CA8D;line-height:1"></i>
+                    <span class="mt10">客户公海池</span>
                 </a>
             </div>
             <div class="mt20" style="height: 10px;background: #F9F9F9;"></div>
             <div class="d-flex flexconm" style="height: 190px;">
                 <div class="flexalone borbottom flexcenter">
                     <a url='/pages/transaction/index' class="flexalone flexcenter">
-                        <div style='width: 100px;'>
+                        <div style='width: 100px;' class="lineCenter">
                             <i class="iconfont f20 iconjiaoyijilu" style="color: #81A4F8;"></i>
                             <span class="ml15">成交记录</span>
                         </div>
                     </a>
                     <div class="flexalone flexcenter">
                         <a url="/pages/contact/index">
-                            <div style='width: 100px;'>
+                            <div style='width: 100px;' class="lineCenter">
                                 <i class="iconfont f20 iconlianxiren" style="color: #00CA8D;"></i>
                                 <span class="ml15">联系人</span>
                             </div>
@@ -36,13 +36,13 @@
                 </div>
                 <div class="flexalone borbottom flexcenter">
                     <div @click="haveNoModule" class="flexalone flexcenter">
-                        <div style='width: 100px;'>
+                        <div style='width: 100px;' class="lineCenter">
                             <i class="iconfont f20 iconsaoyisaojianjieban" style="color: #8E54ED;"></i>
                             <span class="ml15">扫名片</span>
                         </div>
                     </div>
                     <div @click="haveNoModule" class="flexalone flexcenter">
-                        <div style='width: 100px;'>
+                        <div style='width: 100px;' class="lineCenter">
                             <i class="iconfont f20 iconmingpian" style="color: #9bb7F9;"></i>
                             <span class="ml15">智能名片</span>
                         </div>
@@ -50,15 +50,17 @@
                 </div>
                 <div class="flexalone borbottom flexcenter">
                     <div @click="haveNoModule" class="flexalone flexcenter">
-                        <div style='width: 100px;'>
+                        <div style='width: 100px;' class="lineCenter">
                             <i class="iconfont f20 iconhaofangtuoiconfont_wodepeixun" style="color: #FF9A26;"></i>
                             <span class="ml15">培训助手</span>
                         </div>
                     </div>
                     <div class="flexalone flexcenter">
                         <a url='/pages/application/enterprise-management/index' style='width: 100px;'>
+                        <div style='width: 100px;' class="lineCenter">
                             <i class="iconfont f20 iconduoren" style="color: #4D7FF5;"></i>
                             <span class="ml15">企业管理</span>
+                        </div>
                         </a>
                     </div>
                 </div>
@@ -92,4 +94,5 @@ export default {
 .flexalone{flex: 1;}
 .flexconm{flex-direction: column;}
 .borbottom{border-bottom: 1px solid #F9F9F9;}
+.lineCenter{display: flex;align-items: center}
 </style>
