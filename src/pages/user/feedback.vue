d5be6f68c0e32894976ffc761bbd51d644722b75
wangyanlong <2631748820@qq.com>
2019-09-09 17:57:36 +0800

    反馈页面样式修改
    Signed-off-by: wangyanlong <2631748820@qq.com>

--- src/pages/user/feedback.vue
+++ src/pages/user/feedback.vue
@@ -4,13 +4,13 @@
         <div class="d-bg-white pl20 pr20 pt15 mb10">
             <div class="f13 d-text-black pb10">请输入您对产品功能建议或发现的系统问题，我们将不断优化您的使用体验，么么哒！</div>
             <div class="textarea-box pb20">
-                <div class="detail-placeholder f13 d-text-gray" v-show='!isHidePlaceholder'>产品使用问题，请您按照如下方式填写：<br /> 1.在哪个界面，操作了什么功能，遇到了什么问题？<br />2.问题发生的频率（偶尔、频繁等描述）<br />3.请上传问题截图，便于我们理解您的问题。（最多只能上传9张图片）</div>
-                <textarea class="textarea-item wfull f13 d-text-gray" @focus='getTextareaInput' v-model='feedbackForm.contents' :maxlength='500' :show-confirm-bar="false" />
-                <div class="f13 d-text-gray ar">{{feedbackForm.contents.length}}/500</div>
+                <div class="detail-placeholder f13 d-text-cgray" v-show='!isHidePlaceholder'>产品使用问题，请您按照如下方式填写：<br /> 1.在哪个界面，操作了什么功能，遇到了什么问题？<br />2.问题发生的频率（偶尔、频繁等描述）<br />3.请上传问题截图，便于我们理解您的问题。（最多只能上传9张图片）</div>
+                <textarea class="textarea-item wfull f13 d-text-cgray" @focus='getTextareaInput' v-model='feedbackForm.contents' :maxlength='500' :show-confirm-bar="false" />
+                <div class="f13 d-text-cgray ar">{{feedbackForm.contents.length}}/500</div>
             </div>
         </div>
 
-        <div class="d-bg-white pl20 pr20 pt10">
+        <div class="d-bg-white pl20 pr20 pt10 pb20 pb50">
             <div class="d-text-black f13 pb15">添加图片[最多只能上传9张图片]</div>
             <m-upload v-model="feedbackForm.picUrls" limit='9'></m-upload>
         </div>
@@ -85,7 +85,7 @@ export default {
 <style scoped lang="scss">
     .feedback-page{
         background: #f2f2f2;
-		padding-bottom: 60px;
+		// padding-bottom: 60px;
     }
 
     .textarea-box{
