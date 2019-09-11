b11ee689126ac65b1429326cf8b917c481cabc5d
wangyanlong <2631748820@qq.com>
2019-09-11 13:46:15 +0800

    登录修改
    Signed-off-by: wangyanlong <2631748820@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,6 @@ export default {
 	/** 以下是应用生命周期 */
 	// 当uni-app 初始化完成时触发（全局只触发一次）
 	onLaunch: function (options) {
-
-	},
-	// 当 uni-app 启动，或从后台进入前台显示
-	onShow: function (options) {
 		let userInfo = this.$local.fetch('userInfo')
 		userInfo && this.$store.commit('setUserInfo', userInfo)
 
@@ -23,6 +19,14 @@ export default {
 		// 用户不存在则跳转到登录页面
 		const token = this.$local.getItem('token')
 		const finger = this.$local.getItem('finger')
+		if (token && finger && userInfo) {
+			this.$routing.switchTab('/pages/chance/index')
+		} else {
+			this.$routing.redirectTo('/pages/login/index')
+		}
+	},
+	// 当 uni-app 启动，或从后台进入前台显示
+	onShow: function (options) {
 		// 加载小程序时来源有三
 		// 1.正常加载scene1001
 		// 2.来自卡片分享scene1044
@@ -32,12 +36,6 @@ export default {
 		} else if (options.scene === 1044) {
 			// 直接放行该页面
 			this.$routing.navigateTo('/pages/application/enterprise-management/organization/invite/invite')
-		} else {
-			if (token && finger && userInfo) {
-				this.$routing.switchTab('/pages/chance/index')
-			} else {
-				this.$routing.redirectTo('/pages/login/index')
-			}
 		}
 	},
 	// 当 uni-app 从前台进入后台
